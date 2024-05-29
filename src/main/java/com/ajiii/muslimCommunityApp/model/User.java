e88cf873509bc4bd4f2a40e8b596799d22bcb99b
salafiaji <salafiaji@gmail.com>
2024-05-29 12:36:45 +0100

    User entity setup

---
+++ src/main/java/com/ajiii/muslimCommunityApp/model/User.java
@@ -0,0 +1,17 @@
+package com.ajiii.muslimCommunityApp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
