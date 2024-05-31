648bae5025b53478367854b044df9b694d039466
salafiaji <salafiaji@gmail.com>
2024-05-31 04:12:48 +0100

    Files

---
+++ src/main/java/com/ajiii/muslimCommunityApp/MuslimCommunityAppApplication.java
@@ -0,0 +1,13 @@
+package com.ajiii.muslimCommunityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuslimCommunityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuslimCommunityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajiii/muslimCommunityApp/MuslimCommunityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajiii.muslimCommunityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuslimCommunityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
